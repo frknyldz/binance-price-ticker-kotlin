dfbb738f8a3208b1a4fbc167c6796aa529215173
Furkan Yıldız <furkan_yildiz@msn.com>
2021-09-29 00:55:05 +0300

    refactor

--- app/src/main/java/com/example/binancepriceticker/MainActivity.kt
+++ app/src/main/java/com/example/binancepriceticker/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
-import com.google.gson.annotations.SerializedName
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,14 +12,13 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-
+import java.io.Serializable
 
 object Constant {
-    var baseUrl: String = "https://api.binance.com"
+    val baseUrl: String = "https://api.binance.com"
 }
 
-object ApiClient {
-
+object BinanceApiClient {
     private var retrofit: Retrofit? = null
 
     fun getClient(): Retrofit {
@@ -33,12 +32,7 @@ object ApiClient {
     }
 }
 
-data class Symbol(
-    @SerializedName("symbol")
-    val symbol: String = "",
-    @SerializedName("price")
-    val price: String = ""
-)
+data class Symbol(val symbol: String,val price:String) : Serializable
 
 interface PriceTickerService {
     @GET("/api/v3/ticker/price")
@@ -48,17 +42,11 @@ interface PriceTickerService {
 class MainActivity : AppCompatActivity() {
 
     private lateinit var priceTickerService: PriceTickerService
-    private lateinit var symbolNameEditText: EditText
-    private lateinit var priceTextView: TextView
-    private lateinit var getPriceButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        priceTickerService = ApiClient.getClient().create(PriceTickerService::class.java)
-        symbolNameEditText = findViewById(R.id.symbolNameEditText) as EditText
-        priceTextView = findViewById(R.id.priceTextView) as TextView
-        getPriceButton = findViewById(R.id.getPriceButton) as Button
+        priceTickerService = BinanceApiClient.getClient().create(PriceTickerService::class.java)
 
         getPriceButton.setOnClickListener(View.OnClickListener {
 
@@ -66,18 +54,16 @@ class MainActivity : AppCompatActivity() {
 
             symbol.enqueue(object : Callback<Symbol> {
                 override fun onFailure(call: Call<Symbol>, t: Throwable) {
-                    Toast.makeText(applicationContext, t.message.toString(), Toast.LENGTH_LONG)
-                        .show()
+                    Toast.makeText(applicationContext, t.message.toString(), Toast.LENGTH_LONG).show()
                     priceTextView.setText("")
                 }
 
                 override fun onResponse(call: Call<Symbol>, response: Response<Symbol>) {
                     if (response.isSuccessful) {
-                        var symbol: Symbol = (response.body() as Symbol?)!!
+                        val symbol: Symbol = (response.body() as Symbol?)!!
                         priceTextView.setText(symbol.price)
                     } else {
-                        Toast.makeText(applicationContext, "Invalid symbol name", Toast.LENGTH_LONG)
-                            .show()
+                        Toast.makeText(applicationContext, response.toString(), Toast.LENGTH_LONG).show()
                         priceTextView.setText("")
                     }
 
